c0c840e65f8bd7d85706434d2204a390722bc5b5
Boris Joffe <boris@joff3.com>
2015-02-21 17:00:13 -0800

    unlock endpoint

--- server.js
+++ server.js
@@ -7,7 +7,9 @@
 		app = express(),
 
 		// config
-		PORT = process.env.PORT || 9000;
+		PORT = process.env.PORT || 9000,
+
+		EDISON_IP = '127.0.0.1';
 
 	app.get('/', function (req, res) {
 		res.send('doorman in node');
@@ -15,7 +17,14 @@
 
 	app.get('/pkg/:id', function (req, res) {
 		var pkgId = req.params.id;
-		res.send(pkgId);
+		res.send('Your package id was: ' + pkgId);
+
+		// request edison to take a picture
+		// send pkg id and picture to user
+	});
+
+	app.get('/unlock/:door', function (req, res) {
+		// send signal to edison to unlock
 	});
 
 	app.listen(PORT);
